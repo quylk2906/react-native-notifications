ebf4252117db0521565d5d340421792de8d9c2e2
quylk2906 <quylk2906@gmail.com>
2025-06-21 17:21:11 +0700

    add build

--- example/index.js
+++
@@ -1,4 +0,0 @@
-import {AppRegistry} from 'react-native';
-import NotificationsExampleApp from './NotificationsExampleApp';
-
-AppRegistry.registerComponent('NotificationsExampleApp', () => NotificationsExampleApp);
